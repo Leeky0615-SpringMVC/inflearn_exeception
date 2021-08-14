f4aef0b8a2067ccfc76ec1ba4e9bcb5c8aaf56ef
Leeky <rbdus7174@naver.com>
2021-08-14 16:05:10 +0900

    Initial commit

---
+++ src/main/java/hello/exeception/ExeceptionApplication.java
@@ -0,0 +1,13 @@
+package hello.exeception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExeceptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/exeception/ExeceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.exeception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
